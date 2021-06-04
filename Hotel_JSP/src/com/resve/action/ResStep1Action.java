b6d297b67e3820c815e228a23abe878be44f2156
“eunsooo” <coocu78@gmail.com>
2021-06-04 16:34:50 +0900

    res_step0.jsp count 함수

--- Hotel_JSP/src/com/resve/action/ResStep1Action.java
+++ Hotel_JSP/src/com/resve/action/ResStep1Action.java
@@ -14,18 +14,18 @@ public class ResStep1Action implements Action {
 		// step0.jsp (날짜, 입력 폼페이지) 에서 넘어온 checkIn, checkOut, adult, child 
 		// 데이터 받아서 step1.jsp 로 넘겨줌
 		
-		String checkIn = request.getParameter("checkIn").trim();
-		String checkOut = request.getParameter("checkOut").trim();
-		int adult = Integer.parseInt(request.getParameter("adult"));
-		int child = Integer.parseInt(request.getParameter("child"));
+		String resIn = request.getParameter("resIn").trim();
+		String resOut = request.getParameter("resOut").trim();
+		String resAdult = request.getParameter("resAdult").trim();
+		String resChild = request.getParameter("resChild").trim();
 		
-		request.setAttribute("checkIn", checkIn);
-		request.setAttribute("checkOut", checkOut);
-		request.setAttribute("adult", adult);
-		request.setAttribute("child", child);
+		request.setAttribute("resIn", resIn);
+		request.setAttribute("resOut", resOut);
+		request.setAttribute("resAdult", resAdult);
+		request.setAttribute("resChild", resChild);
 		
-		System.out.println(checkIn + checkOut);
-		System.out.println(adult + child);
+		System.out.println(resIn + resOut);
+		System.out.println(resAdult + resChild);
 		
 		
 		ActionForward forward = new ActionForward();		
