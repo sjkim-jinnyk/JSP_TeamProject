27b3e25afa10c88d7c1dac6bd2faa2b4dc3dfdbf
sujinkim <sjkim.jinnyk@gmail.com>
2021-06-08 11:42:29 +0900

    ajax test2

--- Hotel_JSP/src/com/admin/action/RoomSearchOkAction.java
+++ Hotel_JSP/src/com/admin/action/RoomSearchOkAction.java
@@ -20,11 +20,29 @@ public class RoomSearchOkAction implements Action {
 		String roomName = request.getParameter("roomName").trim();
 		String checkIn = request.getParameter("checkIn").trim();	
 		String checkOut = request.getParameter("checkOut").trim();	
+		String search_room = "";
+		String room_image = "";
+		String room_content = "";
+		double room_size = 0;
+		int room_price = 0;
 		
 		ReserveDAO dao = ReserveDAO.getInstance();
 		List<ReserveDTO> list = dao.getInfo(roomName, checkIn, checkOut);
 
-		request.setAttribute("list", list);
+		// request.setAttribute("list", list);
+		
+		for(int i=0; i<list.size(); i++) {
+			search_room = list.get(i).getRoomName();
+		}
+		
+		RoomDAO listdao = RoomDAO.getInstance();
+		String room_detail = listdao.getRoomDetail(search_room);
+		
+		for(int i=0; i<room_detail.size(); i++) {
+			room_image = room_detail.get(i).getRoomImage();
+			room_size = room_detail.get(i).getRoomSize();
+			room_price = room_detail.get(i).getRoomPrice();
+		}
 		
 		ActionForward forward = new ActionForward();
 		forward.setRedirect(false);
