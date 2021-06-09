bbe9941ff3af5e28f4e08bc6a3ee2fb316f077e8
pcs05 <pcs05@59.6.176.107>
2021-06-09 15:40:14 +0900

    DB수정, DB에 맞게 DTO 수정, 삭제

--- Hotel_JSP/src/com/hotel/model/ReserveDTO.java
+++ Hotel_JSP/src/com/hotel/model/ReserveDTO.java
@@ -4,13 +4,16 @@ public class ReserveDTO {		// 예약 DTO
 	private int resNo;			// 예약 번호
 	private String userId;		// 회원 아이디
 	private String roomName;	// 객실 이름
+	private int roomNumber;		// 객실 번호
 	private String resDate;		// 예약 날짜(sysdate)
 	private String resIn;		// 체크인 날짜
 	private String resOut;		// 체크아웃 날짜
+	private String resNod;		// 숙박일수(NOD : Number of Day의 약자)
 	private int resAdult;		// 어른 인원수
 	private int resChild;		// 어린이 인원수
 	private int resAdultBr;		// 어른 조식 갯수
 	private int resChildBr;		// 어린이 조식 갯수
+	private int resBed;			// 엑스트라 베드 갯수
 	private int resTotal;		// 총 가격
 	private String resRequest;	// 요청사항
 	
@@ -32,6 +35,12 @@ public class ReserveDTO {		// 예약 DTO
 	public void setRoomName(String roomName) {
 		this.roomName = roomName;
 	}
+	public int getRoomNumber() {
+		return roomNumber;
+	}
+	public void setRoomNumber(int roomNumber) {
+		this.roomNumber = roomNumber;
+	}
 	public String getResDate() {
 		return resDate;
 	}
@@ -49,6 +58,12 @@ public class ReserveDTO {		// 예약 DTO
 	}
 	public void setResOut(String resOut) {
 		this.resOut = resOut;
+	}
+	public String getResNod() {
+		return resNod;
+	}
+	public void setResNod(String resNod) {
+		this.resNod = resNod;
 	}
 	public int getResAdult() {
 		return resAdult;
@@ -74,6 +89,12 @@ public class ReserveDTO {		// 예약 DTO
 	public void setResChildBr(int resChildBr) {
 		this.resChildBr = resChildBr;
 	}
+	public int getResBed() {
+		return resBed;
+	}
+	public void setResBed(int resBed) {
+		this.resBed = resBed;
+	}
 	public int getResTotal() {
 		return resTotal;
 	}
@@ -86,5 +107,7 @@ public class ReserveDTO {		// 예약 DTO
 	public void setResRequest(String resRequest) {
 		this.resRequest = resRequest;
 	}
+	
+	
 
 }
