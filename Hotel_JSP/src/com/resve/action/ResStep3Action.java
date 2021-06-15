77854b06f56e50050e84e02adaf79d505d88ac22
“eunsooo” <coocu78@gmail.com>
2021-06-15 17:07:35 +0900

    세션 값넘김 정리

--- Hotel_JSP/src/com/resve/action/ResStep3Action.java
+++ Hotel_JSP/src/com/resve/action/ResStep3Action.java
@@ -2,6 +2,7 @@ package com.resve.action;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import com.hotel.controller.Action;
 import com.hotel.controller.ActionForward;
@@ -10,61 +11,33 @@ public class ResStep3Action implements Action {
 
 	@Override
 	public ActionForward execute(HttpServletRequest request, HttpServletResponse response) {
-		// step2.jsp(옵션 선택 폼) 에서 넘어온 
-		// checkIn, checkOut, adult, child,
-		// room_name, room_price
 
-		// resDate, resTotal, adult_br, child_br, extraBed, request 
-		// 데이터 받아서 step3.jsp 로 넘겨줌
+		HttpSession session = request.getSession();
 		
-
-		String checkIn = request.getParameter("checkIn").trim();
-		String checkOut = request.getParameter("checkOut").trim();
-		int adult = Integer.parseInt(request.getParameter("adult"));
-		int child = Integer.parseInt(request.getParameter("child"));
-		
-		String roomId = request.getParameter("roomId").trim();
-		int roomPrice = Integer.parseInt(request.getParameter("roomPrice"));
-		
-		String resDate = request.getParameter("resDate").trim();
-		String resTotal = request.getParameter("resTotal").trim();
-		int adult_br = Integer.parseInt(request.getParameter("adult_br"));
-		int child_br = Integer.parseInt(request.getParameter("child_br"));
-		int extraBed = Integer.parseInt(request.getParameter("extraBed"));
+		String roomTotal = request.getParameter("roomTotal");
+		String resAdultBr = request.getParameter("resAdultBr");
+		String resChildBr = request.getParameter("resChildBr");
+		String resBed = request.getParameter("resBed");
 		String resRequest = request.getParameter("resRequest");
+		String resTotal = request.getParameter("resTotal");
 		
+		session.setAttribute("roomTotal", roomTotal); // 방 가격 * 숙박일수
+		session.setAttribute("resAdultBr", resAdultBr); // 어른 조식
+		session.setAttribute("resChildBr", resChildBr); // 아이 조식
+		session.setAttribute("resBed", resBed);		// 엑스트라 베드 수
+		session.setAttribute("resRequest", resRequest);		// 문의사항
+		session.setAttribute("resTotal", resTotal); // 총금액
 		
-		request.setAttribute("checkIn", checkIn);
-		request.setAttribute("checkOut", checkOut);
-		request.setAttribute("adult", adult);
-		request.setAttribute("child", child);
-		
-		request.setAttribute("roomId", roomId);
-		request.setAttribute("roomPrice", roomPrice);
-		
-		request.setAttribute("resDate", resDate);
-		request.setAttribute("resTotal", resTotal);
-		request.setAttribute("adult_br", adult_br);
-		request.setAttribute("child_br", child_br);
-		request.setAttribute("extraBed", extraBed);
-		request.setAttribute("resRequest", resRequest);
-		
-		
-		System.out.println(checkIn + checkOut);
-		System.out.println(adult + child);
-		System.out.println(roomId);
-		System.out.println(roomPrice);
-		System.out.println(resDate);
-		System.out.println(resTotal);
-		System.out.println(adult_br);
-		System.out.println(child_br);
-		System.out.println(extraBed);
+		System.out.println(roomTotal);
+		System.out.println(resAdultBr);
+		System.out.println(resChildBr);
+		System.out.println(resBed);
 		System.out.println(resRequest);
-		
+		System.out.println(resTotal);
 		
 		ActionForward forward = new ActionForward();		
 		forward.setRedirect(false);
-
+		
 		forward.setPath("/view/resve/res_step3.jsp");				
 		return forward;
 	}
